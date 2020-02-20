12febb67962ccfc4d1db856e04e7a05e7bf6c348
Fnowa <f.nowak@microsoft.wsei.edu.pl>
2020-02-20 18:51:51 +0100

    Wersja 2

---
+++ Zgadywanka/ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Gra w za dużo za mało");
+
+
+            //
+            //1. komputer losuje
+            //
+           
+            Random los = new Random(); // Tworzę obiekt typu Random
+            int Wylosowana = los.Next(1, 101);
+            Console.WriteLine(Wylosowana);
+            Console.WriteLine("Wylosowałem liczbę od 1 do 100.\nOdgadnij ją.");
+           
+            //
+            //2. człowiek zgaduje
+            //
+           
+            Console.Write("Podaj odpowiedź: ");
+            int Propozycja = int.Parse(Console.ReadLine());
+
+            //
+            //3. komputer ocenia
+            //
+            
+            if (Propozycja < Wylosowana)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Za mało");
+                Console.ResetColor();
+            }
+            else if (Propozycja > Wylosowana)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Za dużo");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Trafiono");
+                Console.ResetColor();
+            }
+
+        }
+    }
+}
