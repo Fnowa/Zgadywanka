baf26ebb93b7bafbca6da92b91e2bcac2115c503
Fnowa <f.nowak@microsoft.wsei.edu.pl>
2020-03-10 20:11:56 +0100

    silnik

---
+++ Zgadywanka/ModelGry/Gra.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ModelGry
+{
+    public class Gra
+    {
+        // dane
+        private readonly int wylosowana; // tylko  i tylko jeden raz przypisuje wartości
+
+        public StanGry Stan { get; private set; }
+
+        public List<Ruch> HistoriaGry { get; }
+        
+        //metody
+        public Gra(int a = 1, int b = 100)
+        {
+            Random rnd = new Random();
+            wylosowana = rnd.Next(a, b + 1);
+            Stan = StanGry.Trwa;
+        }
+
+        public int Wylosowana
+        {
+            get
+            {
+                if (Stan == StanGry.Zakonczona)
+                    return wylosowana;
+                else throw new NotSupportedException("W trakcie gry nie dostaniesz tej informacji");
+            }
+        }
+
+        public ODP Odpowiedz(int propozycja)
+        {
+            if (wylosowana < propozycja)
+                return ODP.ZaMalo;
+            else if (wylosowana > propozycja)
+                return ODP.ZaDuzo;
+            else
+            {
+                Stan = StanGry.Zakonczona;
+                return ODP.Trafiono;
+
+            }
+        }
+
+        public void Poddaj()
+        {
+            Stan = StanGry.Zakonczona;
+        }
+
+
+    }
+
+    public enum StanGry
+    {
+        Rozpoczenta,
+        Trwa,
+        Zapauzowana,
+        Zakonczona
+    }
+
+    public enum ODP
+    {
+        ZaMalo = -1,
+        Trafiono = 0,
+        ZaDuzo = 1
+    }
+}
---
+++ Zgadywanka/ModelGry/Ruch.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ModelGry
+{
+    public class Ruch
+    {
+        public readonly int Propozycja;
+        public readonly ODP Odpowiedz;
+        public readonly DateTime Kiedy;
+
+        public Ruch(int prop, ODP odp)
+        {
+            Propozycja = prop;
+            Odpowiedz = odp;
+            Kiedy = DateTime.Now;
+        }
+
+        public override string ToString() => $"{Propozycja} | {Odpowiedz} | {Kiedy}";
+        
+    }
+}
